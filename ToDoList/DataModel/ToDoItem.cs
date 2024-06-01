bc9b8592305e6a0e8965f558b10d9cbc990a2a62
Marcos Leite <m.abner@live.co.uk>
2024-06-01 20:45:00 -0300

    Todo: View Model to Model Dependency

---
+++ ToDoList/DataModel/ToDoItem.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ToDoList.DataModel;
+
+public class ToDoItem
+{
+    public string Description { get; set; } = String.Empty;
+    public bool IsChecked { get; set; }
+}
---
+++ ToDoList/Services/ToDoListService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using ToDoList.DataModel;
+
+namespace ToDoList.Services;
+
+public class ToDoListService
+{
+    public IEnumerable<ToDoItem> GetItems() => new[]
+    {
+        new ToDoItem { Description = "Test the water" },
+        new ToDoItem { Description = "20% water change" },
+        new ToDoItem { Description = "Clean the glass", IsChecked = true },
+    };
+}
